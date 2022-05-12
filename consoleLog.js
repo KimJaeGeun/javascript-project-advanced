c990220700b062e6e21d594a862038bf40542512
KimJaeGeun <99818364+KimJaeGeun@users.noreply.github.com>
2022-05-12 23:52:55 +0900

    객체 상속 공부중(1)

--- consoleLog.js
+++ consoleLog.js
@@ -1,8 +1,44 @@
 import { changeProperty } from './type_syntax/function.js'
-import { duplicationFunc } from './object_function/function.js'
+import { duplicationFunc, protoObjectType, protoFunctionType } from './object_function/function.js'
 
 function consoleLog() {
-    const x = 1;
-    duplicationFunc((x)=> x*2);
+const list = [{
+    no: 1,
+    name: 'tgbnhy',
+    option: false,
+    value: []
+},
+{
+    no: 1,
+    name: 'qwer',
+    option: true,
+    value: [1]
+},
+{
+    no: 1,
+    name: 'asdf',
+    option: false,
+    value: []
+},
+{
+    no: 1,
+    name: 'abc',
+    option: false,
+    value: [1,4,8]
+},
+{
+    no: 1,
+    name: 'zxcv',
+    option: true,
+    value: []
+}]
+
+const testFunc = { test: console.log("test")};
+testFunc.__proto__ = protoObjectType;
+console.log(testFunc.sortName(list));
+
+const testFunc2 = function() { test: console.log("test")};
+// testFunc2.__proto__ = protoFunctionType;
+console.log(protoFunctionType().sortName(list))
 }
 export default consoleLog;
--- object_function/function.js
+++ object_function/function.js
@@ -11,4 +11,23 @@ function duplicationFunc(f) {
     console.log(memorise(f)());
 }
 
-export { duplicationFunc }
+// 사용되는 list가 정해져있는 데이터라 가정할때 프로토타입 함수를 작성하여 상속
+// list = [{
+//     no: 1,
+//     name: 'abc',
+//     option: false,
+//     value: []
+// }]
+const protoObjectType = {
+    sortName: (list) => list.sort((a, b) => a.name - b.name),
+    valueFilterList: (list) => list.filter(item => item.value.length !== 0),
+    optionFilterList: (list) => list.filter(item => item.option)
+}
+
+const protoFunctionType = function() {
+    sortName: (list) => list.sort((a, b) => a.name - b.name);
+    valueFilterList: (list) => list.filter(item => item.value.length !== 0);
+    optionFilterList: (list) => list.filter(item => item.option);
+}
+
+export { duplicationFunc, protoObjectType, protoFunctionType }
