62c3de25c764e75b17ab9b2c9bbf7d7d9d80e38e
KimJaeGeun <99818364+KimJaeGeun@users.noreply.github.com>
2022-05-19 23:08:30 +0900

    객체 공부 완료

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import consoleLog from "./consoleLog.js";
 
 function app() {
-    consoleLog();
+    // consoleLog();
+    document.getElementById("qwer").style.backgroundColor= "red";
 }
 
 export default new app()
