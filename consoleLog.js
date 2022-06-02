b9273f56ec64e238098e740137a55c7bd3d0cf7d
KimJaeGeun <99818364+KimJaeGeun@users.noreply.github.com>
2022-06-03 00:31:13 +0900

    2독 > 객체(2)

--- consoleLog.js
+++ consoleLog.js
@@ -1,9 +1,9 @@
 import { changeProperty } from './type_syntax/function.js'
 import { duplicationFunc, protoObjectType, protoFunctionType } from './object_function/function.js'
-import { basicObjFunc } from './object_function/objectFunctions.js'
+import { basicObjFunc, objFunc } from './object_function/objectFunctions.js'
 import { sortFunc } from './research.js'
 
 function consoleLog() {
-    basicObjFunc();
+    objFunc();
 }
 export default consoleLog;
--- object_function/objectFunctions.js
+++ object_function/objectFunctions.js
@@ -28,8 +28,43 @@ function basicObjFunc() {
 function objFunc() {
     // 오브젝트내 내장 함수
     // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Object
+    const copyObj1 = {...basicObj};
+    const copyObj2 = {
+        contury: "korea"
+    };
+
+    const entriesObj = Object.entries(copyObj1);
+    // [key, value]로 이루어진 2중 배열반환
+    // [[key1, value1], [key2, value2], [key3, value3], [key4, value4]]
+    // for in으로 객체내 키를 순회하였다면, Object.entries로 반환된 배열로 for of구문을 이용한 key, value순회가 가능
+    console.log(entriesObj);
+
+    // key, value로 이루어진 배열을 객체화
+    // Object.entries()의 반환값의 이전값으로 반환
+    const fromEntries = Object.fromEntries(entriesObj);
+    console.log(fromEntries);
+
+    const assignsObj = Object.assign(copyObj1, copyObj2);
+    // 열거 가능한 객체의 복사
+    // 반환 객체는 대상들의 객체를 서로 합쳐만들어진 객체를 반환
+    console.log(assignsObj)
+
+    // 객체는 참조형이기에 값을 비교할 수 없다.(같은 값이라도 같은 곳을 참조하지 않는 한 다르다.)
+    // 객체의 비교의 경우 JSON.stringify등으로 변환후 비교
+    const t1 = {a: "A"};
+    const t2 = {a: "A"};
+
+    console.log(t1 === t2)
+    // 두 값을 비교하여 boolean값을 반환
+    console.log( Object.is(t1, t2))
+
+    // 객체내 key으로 이루어진 배열 반환
+    console.log(Object.keys(copyObj1))
+
+    // 객체내 value로 이루어진 배열 반환
+    console.log(Object.values(copyObj1));
 
 
 }
 
-export { basicObjFunc }
+export { basicObjFunc, objFunc }
